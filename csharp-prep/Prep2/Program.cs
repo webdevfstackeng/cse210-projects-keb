c5b33e7cdac1b0f1b92d2c50f57ef20b9d4aa0d3
webdevfstackeng <u175797830@byui.edu>
2023-01-07 22:01:51 +0200

    Prep1 and 2 uploaded

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string percentageMark = Console.ReadLine();
+        int score = int.Parse(percentageMark);
+
+        string letter = " ";
+
+        if (score >= 90)
+        {
+            letter  = "A";
+        }
+
+        else if (score >= 80)
+        {
+            letter  = "B";
+        }
+
+        else if (score >= 70)
+        {
+            letter  = "C";
+        }
+
+        else if (score >= 60)
+        {
+            letter  = "D";
+        }
+
+        else
+        {
+            letter  = "F";
+        }
+        Console.WriteLine($"Your grade is: {letter}.");
+
+        if (score >= 70)
+        {
+        Console.WriteLine($"Congratulations!, you have passed.");
+        }
+
+        else
+        {
+            Console.WriteLine($"Good luck next time.");
+        }
+
     }
+
+    
 }
